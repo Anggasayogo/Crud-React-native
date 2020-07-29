28a93d3fc51d5d13ca355c8c4abe00d8947a6e6e
anggasayogo <anggasayogosm@gmail.com>
2020-07-29 14:32:15 +0700

    initial foldername

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Router from './router'
+
+const App = () => {
+  return (
+    <View>
+      <Router/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
